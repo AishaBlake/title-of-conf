7dce15cff7cddbeab66ca6b059e768952e4faec4
Aisha Blake <aisha.g.blake@gmail.com>
2019-10-12 23:44:52 -0400

    Fix email list link

--- src/components/hero.js
+++ src/components/hero.js
@@ -27,7 +27,7 @@ const Hero = () => {
             }}>
                 <Logotype />
                 <p>Spring 2020 | Detroit, MI</p>
-                <Link 
+                <a 
                     href="https://titleofconf.us4.list-manage.com/subscribe?u=546218e6c599e2caa7de3c359&id=bc02d38623"
                     style={{
                     border: `3px solid #FEB85A`,
@@ -38,7 +38,7 @@ const Hero = () => {
                     textDecoration: `none`,
                     }}>
                     Subscribe for Updates
-                </Link>
+                </a>
             </div>
             <div style={{
                 width: `50%`
