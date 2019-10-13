f5c181a10061a56431cef8ffe21cf37851fb7b62
Aisha Blake <aisha.g.blake@gmail.com>
2019-10-13 05:52:00 -0400

    Add Patrons

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -7,8 +7,14 @@ import TextBlock from "../components/textBlock"
 const Sponsor = () => (
   <Layout>
     <SEO title="Sponsor" />
-    <TextBlock bgColor="#E7ECEF" title="Sponsors" subtitle="Thank you  for partnering with us">
+    <TextBlock bgColor="#E7ECEF" title="Sponsors" subtitle="Thank you for partnering with us">
       &lt;title of conf&gt; is run entirely by volunteers, so we need your help to make sure it happens! For information on sponsorships, <a href="mailto:aisha@titleofconf.org">contact Aisha</a>. If you're an individual and would like to contribute to the success of the event, <a href="https://www.patreon.com/titleofconf">become a Patron</a> today! 
+    </TextBlock>
+    <TextBlock title="Patrons">
+      <ul>
+        <li>István "Flaki" Szmozsánszky</li>
+        <li>Leslie Vernon-Blake</li>
+      </ul>
     </TextBlock>
   </Layout>
 );
