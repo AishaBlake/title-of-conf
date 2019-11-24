9488ef19d453f1e2ee2ef1b81013c29392dece56
Aisha Blake <aisha.g.blake@gmail.com>
2019-11-24 01:39:28 -0500

    Update code of conduct with Novella's edits

--- src/pages/code-of-conduct.js
+++ src/pages/code-of-conduct.js
@@ -10,52 +10,72 @@ const CodeOfConduct = () => (
       padding: `5% 10%`
     }}>
         <h1>Code of Conduct</h1>
-        <p>&lt;title of conf&gt; is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion. We do not tolerate harassment of conference participants in any form. Conference participants violating these rules may be sanctioned or expelled from the conference without a refund at the discretion of the conference organizers.</p>
+        <p>This Code of Conduct is a set of guidelines about what kinds of behavior will not be tolerated within spaces related to &lt;title of conf&gt;. We have a Code of Conduct because it is necessary and because it is part of our effort to create a safe and harassment-free conference experience. Those found violating this Code of Conduct may be warned or potentially asked to leave the event.</p>
+
+        <h2>Who and what is bound by this Code of Conduct?</h2>
+        <p>Everyone involved with &lt;title of conf&gt; is expected to adhere to this Code of Conduct. This includes, but isn’t limited to, attendees, volunteers, vendors, and guests. The Code of Conduct applies to both online and in-person communication, and includes, for example, things like slides, emails, online forums, booths, stickers, clothing, social media, and recorded talks. Anyone who violates this code of conduct may be expelled from these spaces at the discretion of the &lt;title of conf&gt; organizing committee.</p>
+
+        <h2>Weapons Policy</h2>
+        <p>No weapons are allowed at conference venues, including but not limited to explosives (including fireworks), guns, and large knives such as those used for hunting or display, as well as any other item used for the purpose of causing injury or harm to others.</p>
+        <p>Anyone seen in possession of one of these items will be asked to leave immediately and will only be allowed to return without the weapon.</p>
+
+        <h2>Alcohol/No-Alcohol Policy</h2>
+        <p>During the conference day, we expect all to comply with our no alcohol policy. There will be an afterparty where alcohol will be available for purchase. Even in spaces where alcohol is permitted, drinking alcohol is not an integral part of the &lt;title of conf&gt; culture. Non-alcoholic drinks will be provided and encouraged. Over-consumption of alcohol/drunkenness is highly discouraged.</p>
+        <p>Attendees are further expected to comply with all state and local laws on this matter.</p>
+
+        <h2>What is unacceptable behavior?</h2>
+        <p>Those involved with &lt;title of conf&gt; are expected to comply with both the letter and the spirit of this Code of Conduct, and are expected to contribute to the creation and maintenance of a harassment-free space.</p>
 
         <p>
             Harassment includes, but is not limited to:
             <ul>
-                <li>Verbal comments that reinforce social structures of domination related to gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age, religion.</li>
-                <li>Sexual images in public spaces</li>
-                <li>Deliberate intimidation, stalking, or following</li>
-                <li>Harassing photography or recording</li>
-                <li>Sustained disruption of talks or other events</li>
-                <li>Inappropriate physical contact</li>
-                <li>Unwelcome sexual attention</li>
-                <li>Advocating for, or encouraging, any of the above behaviour</li>
+              <li>Offensive comments related to gender, gender identity and expression, sexual orientation, disability, mental illness, neuro(a)typicality, physical appearance, body size, age, race, or religion.</li>
+              <li>Unwelcome comments regarding a person’s lifestyle choices and practices, including those related to food, health, parenting, drugs, technology, level of experience, career, career stage, and employment.</li>
+              <li>Deliberate misgendering or use of ‘dead’ or rejected names.</li>
+              <li>Gratuitous or off-topic sexual images or behaviour in spaces where they’re not appropriate.</li>
+              <li>Physical contact and simulated physical contact (eg, textual descriptions like “<em>hug</em>” or “<em>backrub</em>”) without consent or after a request to stop.</li>
+              <li>Violence or threats of violence.</li>
+              <li>Incitement of violence towards any individual, including encouraging a person to commit suicide or to engage in self-harm.</li>
+              <li>Stalking or following.</li>
+              <li>Harassing photography or recording, including logging online activity for harassment purposes.</li>
+              <li>Sustained disruption of discussion.</li>
+              <li>Unwelcome sexual attention.</li>
+              <li>Pattern of inappropriate social contact, such as requesting/assuming inappropriate levels of intimacy with others</li>
+              <li>Continued one-on-one communication after requests to cease.</li>
+              <li>Deliberate “outing” of any aspect of a person’s identity without their consent except as necessary to protect vulnerable people from intentional abuse.</li>
+              <li>Publication of non-harassing private communication.</li>
             </ul>
-        </p>
-        <h2>Enforcement</h2>
-        <p>
-            Participants asked to stop any harassing behavior are expected to comply immediately. Vendors conducting related events and sponsors running booths, or similar activities are also subject to the anti-harassment policy. In particular, sponsors should not use sexualized images, activities, or other material. Booth staff (including volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create a sexualized environment.
-        </p>
-        <p>If a participant engages in harassing behaviour, event organisers retain the right to take any actions to keep the event a welcoming environment for all participants. This includes warning the offender or expulsion from the conference with no refund.</p>
-
-        <p>Event organisers may take action to redress anything designed to, or with the clear impact of, disrupting the event or making the environment hostile for any participants.</p>
-        <p>We expect participants to follow these rules at all event venues and event-related social activities. We think people should follow these rules outside event activities too!</p>
-        <h2>Reporting</h2>
-        <p>If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible. Conference staff can be identified by their staff badges. Harassment and other code of conduct violations reduce the value of our event for everyone. We want you to be happy at our event. People like you make our event a better place.</p>
-        <p>You can make a report either personally or anonymously.</p>
-        <h2>Anonymous Report</h2>
-        {/* <p>You can make an anonymous report here [Wufoo form or similar that DOES NOT require an email address to submit.</p> */}
-        <p>We can't follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.</p>
-        <h2>Personal Report</h2>
-        <p>You can make a personal report by:
+            <p>Danger is not the same as discomfort. &lt;title of conf&gt; prioritizes marginalized people’s safety over privileged people’s comfort. &lt;title of conf&gt; reserves the right not to act on complaints regarding:</p>
             <ul>
-            {/* <li>Calling or messaging this phone number: [Phone number for reporting]. This phone number will be continuously monitored for the duration of the event.</li> */}
-            <li>Contacting a staff member, identified by STAFF badges.</li>
+              <li>‘Reverse’ -isms, including ‘reverse racism,’ ‘reverse sexism,’ and ‘cisphobia’</li>
+              <li>Reasonable communication of boundaries, such as “leave me alone,” “go away,” or “I’m not discussing this with you”</li>
+              <li>Communicating in a ‘tone’ you don’t find congenial</li>
+              <li>Criticizing racist, sexist, cissexist, or otherwise oppressive behavior or assumptions</li>
             </ul>
         </p>
-        <p>When taking a personal report, our staff will ensure you are safe and cannot be overheard. They may involve other event staff to ensure your report is managed properly. Once safe, we'll ask you to tell us about what happened. This can be upsetting, but we'll handle it as respectfully as possible, and you can bring someone to support you. You won't be asked to confront anyone and we won't tell anyone who you are.
+        <h2>What if someone breaks the Code of Conduct?</h2>
+        <p>
+          If you are being harassed, or if you notice that someone else is being harassed, or if you have any other concerns, please contact the &lt;title of conf&gt; organizers. If the person who is harassing you is on the team, they will recuse themselves from handling your incident. We will respond as promptly as we can. If a participant asks you to cease a harassing behavior, do so immediately and respectfully.
         </p>
-{/* Our team will be happy to help you contact hotel/venue security, local law enforcement, local support services, provide escorts, or otherwise assist you to feel safe for the duration of the event. We value your attendance.
-[Email address for organizers]
-[Phone number for conference security or organizers]
-[Phone number for hotel/venue security]
-[Local law enforcement]
-[Local sexual assault hot line]
-[Local emergency and non-emergency medical (e.g., urgent care, day clinic)]
-[Local taxi company] */}
+        <p>
+          If a participant engages in harassing behavior, &lt;title of conf&gt; organizers may take any action they deem appropriate, up to and including permanent expulsion from all &lt;title of conf&gt; spaces without a refund and identification of the participant as a harasser to other &lt;title of conf&gt; members or the general public.
+        </p>
+
+        <h2>How do I report violations?</h2>
+        <p>
+          &lt;title of conf&gt; organizers are available via email at [report@titleofconf.org](mailto:report@titleofconf.org). During the conference, we will be wearing organizer t-shirts, which we'll call attention to during our opening remarks and in pre-conference communications. We will respond as quickly as we can.
+        </p>
+        <p>
+          When you report a violation of the Code of Conduct, we pledge to ensure your privacy during the report, we pledge to keep both the reporters’ and the victims’ information confidential (unless you explicitly agree otherwise). We pledge, if appropriate, to help you escalate the report to the applicable authorities, such as venue management, law enforcement, etc.
+        </p>
+        <p>
+          At our discretion, we may publicly name a person about whom we’ve received harassment complaints, or privately warn third parties about them, if we believe that doing so will increase the safety of &lt;title of conf&gt; participants the general public. Additionally, in order to protect volunteers from abuse and burnout, we reserve the right to reject any report we believe to have been made in bad faith.
+        </p>
+
+        <h2>What sources did you use for this Code of Conduct?</h2>
+        <p>
+          For much of this document, we remixed a Code of Conduct one of the organizers made for a previous conference. We drew heavily from GeekFeminism’s <a href="https://geekfeminism.org/about/code-of-conduct/">Code of Conduct</a> and <a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy">Code of Conduct for conferences</a>. We also drew inspiration from <a href="https://www.afrotechfest.co.uk/coc/">Afrotech Fest 2019</a>, <a href="https://codeandsupply.co/policies/conduct">Code &amp; Supply</a>, and <a href="http://confcodeofconduct.com/">Conference Code of Conduct.</a>
+        </p>
     </section>
   </Layout>
 );
