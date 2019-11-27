4164496ae47de139e4e7a4ea63f51777443bc4bf
Aisha Blake <aisha.g.blake@gmail.com>
2019-11-26 21:00:41 -0500

    Fix sponsor prospectus link

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -8,7 +8,7 @@ const Sponsor = () => (
   <Layout>
     <SEO title="Sponsor" />
     <TextBlock bgColor="#E7ECEF" title="Sponsors" subtitle="Thank you for partnering with us">
-      &lt;title of conf&gt; is run entirely by volunteers, so we need your help to make sure it happens! For information on sponsorships, <a href="/images/toc-prospectus.pdf" download>download our prospectus</a>. If you're an individual and would like to contribute to the success of the event over time, <a href="https://www.patreon.com/titleofconf">become a Patron</a> today! 
+      &lt;title of conf&gt; is run entirely by volunteers, so we need your help to make sure it happens! For information on sponsorships, <a href="https://drive.google.com/file/d/1Rg9xVT1qO1IBum5ppsD6GCVeLsNrGA_X/view?usp=sharing">download our prospectus</a>. If you're an individual and would like to contribute to the success of the event over time, <a href="https://www.patreon.com/titleofconf">become a Patron</a> today! 
     </TextBlock>
     <TextBlock title="Patrons">
       <ul>
