dd490b35b3fe1dc5d79f142f875369ed3e570d33
Aisha Blake <aisha.g.blake@gmail.com>
2019-12-04 16:11:05 -0500

    Add patron

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -12,6 +12,7 @@ const Sponsor = () => (
     </TextBlock>
     <TextBlock title="Patrons">
       <ul>
+        <li>Jay Martindell</li>
         <li>István "Flaki" Szmozsánszky</li>
         <li>Leslie Vernon-Blake</li>
       </ul>
